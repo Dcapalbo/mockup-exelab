9cd55f8a9469c42b2ce5dd817652cdd371b213ae
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-20 21:29:01 +0100

    mockup-exelab
    structuring-html-scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,26 @@
 @extends('layout.app')
 @section('content')
-ciao
+<section class="first_section">
+    <div class="higher_first_section">
+        <img src="" alt="Brand_diamond_img">
+    </div>
+    <div class="wrapper">
+        <div class="first_section_left_block">
+            <img src="" alt="Credit_Card_img">
+        </div>
+        <div class="first_section_right_block">
+            <div class="first_right_item">
+                <h2 class="first_right_text"></h2>
+                <h4 class="first_right_text"></h4>
+                <p class="first_right_text"></p>
+            </div>
+            <div class="first_right_item">
+                <button class="first_right_button"></button>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="second_section">
+
+</section>
 @endsection
