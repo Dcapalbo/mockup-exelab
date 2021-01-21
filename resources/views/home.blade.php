f6398ce130099402e7cf450977aa900f9a177798
Domenico Capalbo <capalbodomenico@gmail.com>
2021-01-21 08:56:23 +0100

    mockup-exelab
    structuring-last-section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,8 +19,94 @@
             </div>
         </div>
     </div>
+    <div class="large_wrapper">
+        <div class="first_right_icon">
+            <img src="" alt="upper_arrow_icon">
+        </div>
+    </div>
 </section>
 <section class="second_section">
+    <div class="right_auto_wrapper">
+        <div class="second_section_left_block">
+            <div class="second_left_block">
+                <div class="higher_left_block">
+                    <div class="higher_left_specific">
+                        <img src="" alt="img_banco_buone_azioni">
+                    </div>
+                    <div class="higher_left_specific">
+                        <h2></h2>
+                    </div>
+                    <div class="higher_left_specific">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="lower_left_block">
+                    <div class="lower_left_specific">
+                        <img src="" alt="">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="lower_left_specific">
+                        <img src="" alt="">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                    <div class="lower_left_specific">
+                        <img src="" alt="">
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="second_section_right_block">
+            <img src="" alt="Picture_of_grandpa">
+        </div>
+    </div>
+</section>
+<section class="third_section">
+    <div class="wrapper">
+        <div class="third_higher_block">
+            <h3></h3>
+            <h1></h1>
+        </div>
+        <div class="third_main_left_block">
+            <div class="third_characteristic">
+                <h2>f</h2>
+                <h4>Flawless</h4>
+                <h5>Si ricarica ad ogni tua buona azione</h5>
+                <h5>Ottima per i tuoi acquisti online</h5>
+                <h5>Facile da usare grazie alla BBA App</h5>
+            </div>
+            <div class="third_main_right_block">
+                <a href="">
+                    <img src="" alt="arrow_left">
+                </a>
+                <div class="third_right_item">
 
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="fourth_section">
+    <div class="lower_wrapper">
+        <div class="form_block">
+            <h1></h1>
+            <p></p>
+            <form action="Get">
+                <p>&#42;Campo obbligatorio</p>
+                <input type="text" placeholder="Nome">
+                <p>&#42;Campo obbligatorio</p>
+                <input type="text" placeholder="Cognome">
+                <p>&#42;Campo obbligatorio</p>
+                <input type="text" placeholder="email">
+                <button></button>
+            </form>
+        </div>
+    </div>
 </section>
 @endsection
